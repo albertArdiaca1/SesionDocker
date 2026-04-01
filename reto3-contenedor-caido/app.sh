eeb870c42c0a6e7a3db384e6c65cc9bcddc04b39
albert.ardiaca <albert.ardiaca@aktios.com>
2026-04-01 11:07:50 +0200

    correcion

---
+++ reto3-contenedor-caido/app.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+echo "Iniciando aplicación..."
+echo "Versión: $APP_VERSION"
+
+if [ -z "$APP_PORT" ]; then
+  echo "ERROR: Variable APP_PORT no definida"
+  exit 1
+fi
+
+echo "Escuchando en puerto $APP_PORT"
+exec nginx -g "daemon off;"
